f7b8a957ba576aa481aa4f927aa93eaca7c59ed0
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-08-21 22:55:53 +0300

    Delete 262-trips-and-users directory

--- 262-trips-and-users/trips-and-users.sql
+++
@@ -1,9 +0,0 @@
-Select request_at as Day, round(sum(case when status = "completed" then 0 else am end)/sum(am),2) as "Cancellation Rate"
-  From
-    (Select request_at, status, count(status) as am
-    From Trips t
-    Where client_id not in (Select users_id From Users Where banned = "Yes") AND
-    driver_id not in (Select users_id From Users Where banned = "Yes")
-    Group by request_at, status) as a
-  Group by Day
-Having day <= "2013-10-03" and day >= "2013-10-01"
