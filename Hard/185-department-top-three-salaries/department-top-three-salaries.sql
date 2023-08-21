1ac75e5e456f934ef730d5bd19a60c97ef8efdf9
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-08-21 23:02:20 +0300

    Time: 2482 ms (31.37%) | Memory: 0B (100.00%) - LeetSync

--- Hard/185-department-top-three-salaries/department-top-three-salaries.sql
+++ Hard/185-department-top-three-salaries/department-top-three-salaries.sql
@@ -2,8 +2,7 @@ SELECT d.name AS Department, e.name AS Employee, e.salary
 FROM Employee AS e
 INNER JOIN Department AS d
 ON d.id = e.departmentId
-WHERE (
-  Select Count(Distinct Salary)
+WHERE (Select Count(Distinct Salary)
   From Employee as r
   where r.salary > e.salary
   and r.DepartmentId = e.DepartmentId) < 3
