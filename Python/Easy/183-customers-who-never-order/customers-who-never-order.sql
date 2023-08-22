5c76f212af04491a54fef2af76a10a18b08bc9cd
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-08-22 18:23:35 +0300

    Update customers-who-never-order.sql

--- Python/Easy/183-customers-who-never-order/customers-who-never-order.sql
+++ Python/Easy/183-customers-who-never-order/customers-who-never-order.sql
@@ -1,9 +1,7 @@
-# Write your MySQL query statement below
-SELECT 
-    name as Customers
-FROM
-    Customers c
-LEFT JOIN 
-    Orders o ON c.Id = o.customerId
-WHERE 
-    o.id is NULL
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    merged_df = pd.merge(customers, orders, how='left', left_on='id', right_on='customerId')
+    customers_without_orders = merged_df[merged_df['customerId'].isna()].rename(columns={"name" : "Customers"})
+    res = customers_without_orders[['Customers']]
+    return res
