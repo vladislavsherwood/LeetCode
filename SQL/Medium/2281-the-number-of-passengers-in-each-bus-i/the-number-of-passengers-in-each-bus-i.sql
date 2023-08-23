5502b1d640d05862491ecd60e54f8029531c5035
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-08-23 04:23:54 +0300

    Time: 1985 ms (62.33%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/2281-the-number-of-passengers-in-each-bus-i/the-number-of-passengers-in-each-bus-i.sql
@@ -0,0 +1,18 @@
+SELECT
+    b.bus_id, COUNT(n.passenger_id) AS passengers_cnt
+FROM
+    (SELECT
+        passenger_id, MIN(b.arrival_time) AS department_time
+    FROM
+        Passengers p
+    LEFT JOIN 
+        Buses b ON p.arrival_time <= b.arrival_time 
+    GROUP BY 
+        passenger_id) AS n
+RIGHT JOIN
+    Buses b ON b.arrival_time = n.department_time
+GROUP BY
+    b.bus_id
+ORDER BY
+    b.bus_id
+
