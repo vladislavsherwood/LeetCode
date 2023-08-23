44aee1f6021716e8d87fa7debf1a9377786a989a
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-08-23 05:02:33 +0300

    Time: 992 ms (35.57%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Easy/1759-find-the-missing-ids/find-the-missing-ids.sql
@@ -0,0 +1,17 @@
+WITH RECURSIVE ids AS
+    (SELECT 
+        1 AS value
+    UNION ALL
+    SELECT 
+        value + 1
+    FROM 
+        ids
+    WHERE 
+        value < (SELECT MAX(customer_id) FROM Customers))
+
+SELECT 
+    value as ids
+FROM 
+    ids
+WHERE
+    value not in (SELECT customer_id FROM Customers)
