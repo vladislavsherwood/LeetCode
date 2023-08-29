1e5a4ff8398259e8a3e916d72a2839bb116a324f
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-08-29 08:19:43 +0300

    Time: 1967 ms (19.47%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/1198-unpopular-books/unpopular-books.sql
@@ -0,0 +1,13 @@
+SELECT 
+    b.book_id, b.name
+FROM
+    Books b
+LEFT JOIN 
+    (SELECT book_id, sum(quantity) as amount
+    FROM Orders 
+    WHERE (dispatch_date) >= "2018-06-23"
+    GROUP BY book_id) o ON b.book_id = o.book_id
+WHERE
+    b.available_from <= '2019-05-23' 
+    AND (amount < 10 or amount is null)
+
