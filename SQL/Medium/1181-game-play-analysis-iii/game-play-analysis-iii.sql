bf92d305b623f61f855b80b76194fe0f50b81467
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-08-30 21:44:01 +0300

    Time: 3382 ms (5.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/1181-game-play-analysis-iii/game-play-analysis-iii.sql
@@ -0,0 +1,12 @@
+SELECT 
+    a1.player_id,
+    a1.event_date,
+    SUM(a1.games_played) OVER(PARTITION BY a1.player_id ORDER BY a1.event_date) as games_played_so_far
+FROM
+    Activity a1, Activity a2
+WHERE
+    a1.event_date <= a2.event_date
+GROUP BY
+    a1.player_id, a1.event_date
+ORDER BY
+     a1.player_id, a1.event_date
