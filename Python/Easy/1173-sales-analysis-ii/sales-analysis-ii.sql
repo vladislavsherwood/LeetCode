9ab93989c83f5a333aecf29d76c65a168623362c
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-08-30 23:53:55 +0300

    Time: 1728 ms (91.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ Python/Easy/1173-sales-analysis-ii/sales-analysis-ii.sql
@@ -0,0 +1,6 @@
+SELECT s.buyer_id
+FROM Sales AS s INNER JOIN Product AS p
+ON s.product_id = p.product_id
+GROUP BY s.buyer_id
+HAVING SUM(CASE WHEN p.product_name = 'S8' THEN 1 ELSE 0 END) > 0
+AND SUM(CASE WHEN p.product_name = 'iPhone' THEN 1 ELSE 0 END) = 0;
