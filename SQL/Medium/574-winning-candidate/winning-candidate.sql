2d99ad18d9a0e4cb727f21172bb6ad6d4cdee557
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-08-31 09:52:07 +0300

    Time: 1536 ms (5.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/574-winning-candidate/winning-candidate.sql
@@ -0,0 +1,17 @@
+SELECT
+    name
+FROM
+    Candidate
+WHERE
+    id =
+        (SELECT
+            candidateId
+        FROM
+            VOTE
+        GROUP BY
+            candidateId
+        ORDER BY
+            COUNT(id) desc
+        LIMIT 1)
+
+
