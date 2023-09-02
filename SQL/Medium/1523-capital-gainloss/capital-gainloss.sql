f356f6a13cf785e032335160dfea41fd3f20367d
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-09-02 16:03:09 +0300

    Time: 1561 ms (8.51%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/1523-capital-gainloss/capital-gainloss.sql
@@ -0,0 +1,8 @@
+SELECT 
+    stock_name,
+    SUM(CASE WHEN operation = "Buy" THEN price * -1
+    WHEN operation = "Sell" THEN price End) as capital_gain_loss
+FROM 
+    Stocks
+GROUP BY 
+    stock_name
