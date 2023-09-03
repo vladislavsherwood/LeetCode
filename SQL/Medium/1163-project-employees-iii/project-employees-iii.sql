1fb0c96a78fab5d8ca8b061b9e104f0ca15aebf4
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-09-03 18:33:04 +0300

    Time: 1266 ms (72.25%) | Memory: 0B (100.00%) - LeetSync

--- SQL/Medium/1163-project-employees-iii/project-employees-iii.sql
+++ SQL/Medium/1163-project-employees-iii/project-employees-iii.sql
@@ -3,9 +3,9 @@ SELECT
 FROM 
     Project p 
 JOIN 
-    Employee e on p.employee_id = e.employee_id
+    Employee e ON p.employee_id = e.employee_id
 WHERE 
-    (p.project_id, e.experience_years) in
+    (p.project_id, e.experience_years) IN
         (SELECT
             p.project_id,
             MAX(experience_years)
