acfc3012bf18b8d21a3210819b9049515b2ce653
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-09-03 18:32:12 +0300

    Time: 1614 ms (21.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/1163-project-employees-iii/project-employees-iii.sql
@@ -0,0 +1,18 @@
+SELECT 
+    p.project_id, p.employee_id
+FROM 
+    Project p 
+JOIN 
+    Employee e on p.employee_id = e.employee_id
+WHERE 
+    (p.project_id, e.experience_years) in
+        (SELECT
+            p.project_id,
+            MAX(experience_years)
+        FROM
+            Project p
+        JOIN 
+            Employee e ON p.employee_id = e.employee_id
+        GROUP BY
+            p.project_id)
+
