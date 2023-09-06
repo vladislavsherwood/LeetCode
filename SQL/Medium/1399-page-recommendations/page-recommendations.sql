92ad3670b4d1264c868334b52f730ed52b83b69a
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-09-06 11:30:31 +0300

    Time: 1595 ms (17.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/1399-page-recommendations/page-recommendations.sql
@@ -0,0 +1,17 @@
+SELECT DISTINCT page_id as recommended_page
+FROM Likes
+WHERE user_id IN
+    (SELECT user2_id
+    FROM Friendship
+    WHERE user1_id = "1"
+    
+    UNION ALL
+
+    SELECT user1_id
+    FROM Friendship
+    WHERE user2_id = "1")
+    
+AND page_id NOT IN 
+    (SELECT page_id
+    FROM Likes
+    WHERE user_id = "1")
