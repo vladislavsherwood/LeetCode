111c392055b76d6754f3ac5af49c6481bb3ad8b3
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-09-06 10:26:35 +0300

    Time: 2484 ms (7.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -0,0 +1,39 @@
+WITH CTE AS (
+    SELECT
+        host_team,
+        guest_team,
+        (CASE WHEN host_goals > guest_goals THEN 3
+        WHEN host_goals = guest_goals THEN 1
+        ELSE 0 END) as host_points,
+        (CASE WHEN host_goals < guest_goals THEN 3
+        WHEN host_goals = guest_goals THEN 1
+        ELSE 0 END) as guest_points
+    FROM
+        Matches),
+CTE2 AS (
+    SELECT 
+        host_team as team_id, SUM(host_points) as num_points
+    FROM 
+        CTE
+    GROUP BY
+        host_team
+
+    UNION ALL
+
+    SELECT 
+        guest_team as team_id, SUM(guest_points) as num_points
+    FROM 
+        CTE
+    GROUP BY
+        guest_team)
+
+SELECT
+    t.team_id, team_name, COALESCE(SUM(num_points),0) as num_points
+FROM
+    CTE2
+RIGHT JOIN
+    Teams t on CTE2.team_id = t.team_id
+GROUP BY
+    t.team_id
+ORDER BY
+    num_points DESC, team_id
