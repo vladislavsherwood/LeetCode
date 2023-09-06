4725b9945455a5ca0a8ac2b1f92734751cf5585b
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-09-06 11:32:21 +0300

    Time: 3087 ms (5.07%) | Memory: 0B (100.00%) - LeetSync

--- SQL/Medium/1399-page-recommendations/page-recommendations.sql
+++ SQL/Medium/1399-page-recommendations/page-recommendations.sql
@@ -1,16 +1,11 @@
 SELECT DISTINCT page_id as recommended_page
 FROM Likes
 WHERE user_id IN
-    (SELECT user2_id
-    FROM Friendship
-    WHERE user1_id = "1"
-    
-    UNION ALL
-
-    SELECT user1_id
-    FROM Friendship
-    WHERE user2_id = "1")
-
+    (SELECT 
+    CASE WHEN user1_id=1 THEN user2_id
+    WHEN user2_id=1 THEN user1_id
+    END
+    FROM Friendship)
 AND page_id NOT IN 
     (SELECT page_id
     FROM Likes
