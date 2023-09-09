2ff1cc090de86e9ab13883474ad857ceaebfd1f6
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-09-10 01:41:39 +0300

    Time: 1418 ms (73.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/1328-monthly-transactions-ii/monthly-transactions-ii.sql
@@ -0,0 +1,62 @@
+WITH CTE AS 
+    (SELECT
+        LEFT(c.trans_date,7) month,
+        country,
+        COUNT(trans_id) chargeback_count,
+        SUM(amount) chargeback_amount
+    FROM
+        Chargebacks c
+    LEFT JOIN 
+        Transactions t ON id = trans_id
+    GROUP BY
+        LEFT(c.trans_date,7), country),
+
+CTE2 as
+    (SELECT 
+        LEFT(trans_date,7) month,
+        country,
+        SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) approved_count,
+        SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) approved_amount
+    FROM 
+        Transactions
+    GROUP BY
+        LEFT(trans_date,7), country),
+
+AllMonths AS (
+    SELECT 
+        LEFT(trans_date, 7) AS month, country
+    FROM 
+        Transactions
+    UNION
+    SELECT
+        LEFT(c.trans_date, 7) AS month, t.country
+    FROM Chargebacks c
+    JOIN Transactions t on id = trans_id 
+),
+
+AllCountries as (
+    SELECT 
+        country
+    FROM
+        Transactions
+)
+
+
+SELECT
+    am.month,
+    am.country,
+    COALESCE(approved_count, 0) AS approved_count,
+    COALESCE(approved_amount, 0) AS approved_amount,
+    COALESCE(chargeback_count, 0) AS chargeback_count,
+    COALESCE(chargeback_amount, 0) AS chargeback_amount
+FROM
+    AllMonths am
+LEFT JOIN
+    CTE ON am.month = CTE.month and am.country = CTE.country
+LEFT JOIN
+    CTE2 ON am.month = CTE2.month and am.country = CTE2.country
+WHERE
+    approved_count <> 0 or chargeback_count <> 0
+ORDER BY
+    am.month, CTE.country;
+
