b664893782a9e65330126a5bfbe7ab1ba72c9a91
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-09-09 23:12:38 +0300

    Time: 800 ms (84.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/1259-article-views-ii/article-views-ii.sql
@@ -0,0 +1,11 @@
+SELECT
+    DISTINCT viewer_id id
+FROM
+    (SELECT
+        viewer_id, view_date, COUNT(DISTINCT article_id) as amount
+    FROM
+        Views
+    GROUP BY
+        viewer_id, view_date
+    HAVING
+        amount > 1) a
