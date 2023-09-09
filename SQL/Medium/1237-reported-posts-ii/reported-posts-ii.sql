46c6d36ffbad6ba36b2514e85222cc7e5cbf9fcd
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-09-09 15:34:57 +0300

    Time: 3958 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- SQL/Medium/1237-reported-posts-ii/reported-posts-ii.sql
+++ SQL/Medium/1237-reported-posts-ii/reported-posts-ii.sql
@@ -1,13 +1,13 @@
 SELECT
-    ROUND(AVG(am)*100,2) as average_daily_percent
+    ROUND(AVG(am)*100,2) AS average_daily_percent
 FROM
     (SELECT
-        COUNT(DISTINCT r.post_id)/COUNT(DISTINCT a.post_id) as am
+        COUNT(DISTINCT r.post_id)/COUNT(DISTINCT a.post_id) AS am
     FROM
         Actions a
     LEFT JOIN 
-        Removals r on a.post_id = r.post_id
+        Removals r ON a.post_id = r.post_id
     WHERE
         extra = "spam"
     GROUP BY
-        action_date) as n
+        action_date) AS n
