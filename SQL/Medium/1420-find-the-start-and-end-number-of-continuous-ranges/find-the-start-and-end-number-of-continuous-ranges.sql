41f160b80790f8a956e29a1c5f138f5a755a7cd0
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-09-11 11:23:21 +0300

    Time: 1103 ms (21.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/Medium/1420-find-the-start-and-end-number-of-continuous-ranges/find-the-start-and-end-number-of-continuous-ranges.sql
@@ -0,0 +1,9 @@
+SELECT 
+    MIN(log_id) start_id, MAX(log_id) end_id
+FROM
+    (SELECT
+        log_id, log_id - ROW_NUMBER() OVER (ORDER BY log_id) as grp
+    FROM
+        Logs) n
+GROUP BY
+    grp
