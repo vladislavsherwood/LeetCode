fe16ddbaf9b8fb5d0e4f4b6c0b15fa4a035e32ca
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-10-05 02:18:07 +0300

    Time: 1050 ms (80.72%) | Memory: 0B (100.00%) - LeetSync

--- SQL/Medium/1852-biggest-window-between-visits/biggest-window-between-visits.sql
+++ SQL/Medium/1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -7,4 +7,4 @@ WITH visits AS (
 SELECT v1.user_id, MAX(DATEDIFF(COALESCE(v2.visit_date,'2021-1-1'),v1.visit_date)) as biggest_window
 FROM visits v1
 LEFT JOIN visits v2 on v1.row_num +1 = v2.row_num and v1.user_id = v2.user_id
-GROUP BY v1.user_id;
+GROUP BY v1.user_id
