86d3cd76c288ef64d7906930830ab395591658e5
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-10-05 03:04:04 +0300

    Time: 1711 ms (94.97%) | Memory: 0B (100.00%) - LeetSync

--- SQL/Medium/1998-suspicious-bank-accounts/suspicious-bank-accounts.sql
+++ SQL/Medium/1998-suspicious-bank-accounts/suspicious-bank-accounts.sql
@@ -11,6 +11,7 @@ WITH incomes as (
   GROUP BY 
     account_id, 
     DATE_FORMAT(day, '%Y-%m')),
+
 filter as(
   SELECT 
     i.account_id, 
@@ -25,5 +26,5 @@ filter as(
 SELECT DISTINCT f1.account_id
 FROM filter f1
 LEFT JOIN filter f2 ON DATE_ADD(CONCAT(f1.month_year,'-01'), INTERVAL 1 MONTH) = CONCAT(f2.month_year,'-01') AND f1.account_id = f2.account_id
-WHERE f1.filter = 'suspicious' AND f2.filter = 'suspicious'
+WHERE f1.filter = 'suspicious' AND f2.filter = 'suspicious';
 
