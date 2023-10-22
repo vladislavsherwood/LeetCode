54bb80073410865e4200050432e72d24ef8c0369
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-10-22 20:50:27 +0300

    Time: 985 ms (62.83%) | Memory: 0B (100.00%) - LeetSync

--- SQL/Medium/1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
+++ SQL/Medium/1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -6,4 +6,4 @@ JOIN
     LogInfo l2 
     ON l1.account_id = l2.account_id 
     AND l1.ip_address <> l2.ip_address
-    AND ((l1.login BETWEEN l2.login AND l2.logout) OR (l1.logout BETWEEN l2.login AND l2.logout))
+    AND ((l1.login BETWEEN l2.login AND l2.logout) OR (l1.logout BETWEEN l2.login AND l2.logout));
