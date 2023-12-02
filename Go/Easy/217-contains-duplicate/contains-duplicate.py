e81b593ab21130c5134641fa5e2edf0f66926d65
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-12-02 15:36:35 +0200

    Time: 485 ms (34.73%) | Memory: 31.1 MB (42.56%) - LeetSync

---
+++ Go/Easy/217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        hash = set()
+
+        for num in nums:
+            if num in hash:
+                return True
+            hash.add(num)
+        return False
+        
+        
+        
