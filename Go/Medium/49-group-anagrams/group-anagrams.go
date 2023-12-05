6fba228cb75c8db6500e85727cab0d8fdc03d8ec
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-12-05 16:30:24 +0200

    Time: 25 ms (40.03%) | Memory: 8.3 MB (37.36%) - LeetSync

--- Go/Medium/49-group-anagrams/group-anagrams.go
+++ Go/Medium/49-group-anagrams/group-anagrams.go
@@ -8,13 +8,12 @@ func groupAnagrams(strs []string) [][]string {
     }
 
     result := make([][]string,0,len(anagrams))
-    
+
     for _, group := range anagrams{
         result = append(result, group)
     }
     return result
 }
-
 
 func sortString (s string) string{
     characters := strings.Split(s,"")
