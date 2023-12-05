9e06f218bd9df8d262e7cdea44d9ca6fbc81087a
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-12-05 16:30:13 +0200

    Time: 24 ms (45.22%) | Memory: 7.9 MB (56.59%) - LeetSync

--- Go/Medium/49-group-anagrams/group-anagrams.go
+++ Go/Medium/49-group-anagrams/group-anagrams.go
@@ -1,12 +1,14 @@
 func groupAnagrams(strs []string) [][]string {
 
     anagrams := make(map[string][]string)
+
     for _, str := range strs{
         sortedStr := sortString(str)
         anagrams[sortedStr] = append(anagrams[sortedStr], str)
     }
 
     result := make([][]string,0,len(anagrams))
+    
     for _, group := range anagrams{
         result = append(result, group)
     }
