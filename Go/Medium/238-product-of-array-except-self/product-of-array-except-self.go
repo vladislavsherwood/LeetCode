05febde7159022f0708afc93d877bf17811fad0b
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-12-07 00:04:20 +0200

    Time: 15 ms (97.75%) | Memory: 7.2 MB (86.63%) - LeetSync

--- Go/Medium/238-product-of-array-except-self/product-of-array-except-self.go
+++ Go/Medium/238-product-of-array-except-self/product-of-array-except-self.go
@@ -1,14 +1,18 @@
 func productExceptSelf(nums []int) []int {
   res := make([]int, len(nums))
+
   prefix := 1
   postfix := 1
+
   for i, num := range nums{
       res[i] = prefix
       prefix *= num
   }
+
 	for i := len(nums) - 1; i >= 0; i-- {
 		res[i] *= postfix
 		postfix *= nums[i]
 	}
+  
   return res
 }
