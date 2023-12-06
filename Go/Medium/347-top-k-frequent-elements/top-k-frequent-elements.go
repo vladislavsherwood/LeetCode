520f88754bd9a07830329eb88988a20e27bf4cc3
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-12-06 17:50:17 +0200

    Time: 14 ms (38.59%) | Memory: 6.1 MB (59.45%) - LeetSync

---
+++ Go/Medium/347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -0,0 +1,22 @@
+func topKFrequent(nums []int, k int) []int {
+    count := make(map[int]int)
+    freq := make([][]int, len(nums)+1)
+
+    for _, n := range nums{
+        count[n] = count[n] + 1
+    }
+
+    for n, c := range count{
+        freq[c] = append(freq[c], n)
+    }
+
+    result := make([]int, 0)
+
+    for i := len(freq) - 1; i > 0; i-- {
+        result = append(result, freq[i]...)
+        if len(result) >= k{
+            return result[:k]
+        }
+    }
+    return result
+}
