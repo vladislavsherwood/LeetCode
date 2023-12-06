bf877a24aeacd11fb9670b23f03869aba29ec4b6
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-12-06 17:54:24 +0200

    Time: 8 ms (89.60%) | Memory: 6.1 MB (59.45%) - LeetSync

--- Go/Medium/347-top-k-frequent-elements/top-k-frequent-elements.go
+++ Go/Medium/347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -11,6 +11,7 @@ func topKFrequent(nums []int, k int) []int {
     }
 
     result := make([]int, 0)
+    
     for i := len(freq) - 1; i > 0; i-- {
         result = append(result, freq[i]...)
         if len(result) >= k{
