96c6cfe8887031d59c165af35976bf6e82145bac
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-12-07 12:30:44 +0200

    Time: 8 ms (7.63%) | Memory: 4.2 MB (11.86%) - LeetSync

--- Go/Medium/36-valid-sudoku/valid-sudoku.go
+++ Go/Medium/36-valid-sudoku/valid-sudoku.go
@@ -11,12 +11,14 @@ func isValidSudoku(board [][]byte) bool {
       if current_value == "."{
         continue
       }
+
       _, check1 := hashMap[current_value + "row f" + string(row)]
       _, check2 := hashMap[current_value + "column f" + string(column)]
       _, check3 := hashMap[current_value + "square f" + string(row/3) + "_" + string(column/3)]
 
       if check1 || check2 || check3{
         return false
+        
       } else{
         hashMap[(current_value + "row f" + string(row))] = true
         hashMap[(current_value + "column f" + string(column))] = true
