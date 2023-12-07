23183f2e8eb65ba766a2b5893b45a292bac70a8e
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-12-07 15:40:08 +0200

    Time: 1922 ms (14.28%) | Memory: 15.1 MB (16.79%) - LeetSync

---
+++ Go/Medium/128-longest-consecutive-sequence/longest-consecutive-sequence.go
@@ -0,0 +1,28 @@
+func longestConsecutive(nums []int) int {
+    set := map[int]bool{}
+
+    for _, n := range nums{
+        set[n] = true
+    }
+
+    res := 0
+
+    for _, n := range nums{
+        if set[n-1]{
+            continue
+        }
+
+        consecutive := 1
+        next := n + 1
+
+        for set[next]{
+            consecutive++
+            next++
+        }
+
+        if consecutive > res{
+            res = consecutive
+        }
+    }
+    return res
+}
