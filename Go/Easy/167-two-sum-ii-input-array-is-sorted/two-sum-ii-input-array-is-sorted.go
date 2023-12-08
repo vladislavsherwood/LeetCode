42d7a3283610e1575fb5ddb993d689dd40ddc7a2
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-12-08 15:10:18 +0200

    Time: 11 ms (65.14%) | Memory: 6.4 MB (6.17%) - LeetSync

--- Go/Easy/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
+++ Go/Easy/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
@@ -1,7 +1,7 @@
 func twoSum(numbers []int, target int) []int {
     for l,r := 0, len(numbers) - 1; l<r;{
-        n := numbers[l] + numbers[r]
 
+        n := numbers[l] + numbers[r]
         if n == target{
             return []int{l+1, r+1}
         }
