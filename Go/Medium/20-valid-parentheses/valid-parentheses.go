ad5ab4a67710cda1468089fabcede1eb9bfdbdb0
Vladislav Sherwood <138873210+vladislavsherwood@users.noreply.github.com>
2023-12-09 18:37:31 +0200

    Time: 1 ms (76.68%) | Memory: 2 MB (56.56%) - LeetSync

--- Go/Medium/20-valid-parentheses/valid-parentheses.go
+++ Go/Medium/20-valid-parentheses/valid-parentheses.go
@@ -1,28 +1,39 @@
 func isValid(s string) bool {
+    // Define the mapping of closed brackets to their corresponding open brackets
     pairs := map[byte]byte{
-        ')':'(',
-        ']':'[',
-        '}':'{',
+        ')': '(',
+        ']': '[',
+        '}': '{',
     }
+    
+    // Initialize an empty stack
     stack := make([]byte, 0)
     
-    for _, char := range []byte(s){
-
+    // Iterate through each character in the input string
+    for _, char := range []byte(s) {
+        // Retrieve the corresponding open bracket for the current closed bracket
         pair, ok := pairs[char]
+        
+        // If the current character is an open bracket, push it onto the stack
         if !ok {
             stack = append(stack, char)
             continue
         }
-
-        if len(stack) == 0{
+    
+        // If the stack is empty (no corresponding open bracket found), return false
+        if len(stack) == 0 {
             return false
         }
 
-        if stack[len(stack)-1] != pair{
+        // Check if the current closed bracket corresponds to the last open bracket in the stack
+        if stack[len(stack)-1] != pair {
             return false
         }
 
+        // Pop the last element from the stack
         stack = stack[:len(stack) - 1]
     }
+    
+    // Check if there are no remaining unclosed open brackets in the stack
     return len(stack) == 0
 }
